6d56af25bba222aaccc8635944dd2f7289446656
Periklis Gkolias <per.gkolias@gmail.com>
2018-10-19 22:00:09 +0300

    RNA complements

---
+++ rna-transcription/rna_transcription.py
@@ -0,0 +1,8 @@
+def to_rna(dna_strand):
+    complements = {'G': 'C',
+                   'C': 'G',
+                   'T': 'A',
+                   'A': 'U'
+                   }
+
+    return ''.join([complements[nucleotide] for nucleotide in dna_strand])
